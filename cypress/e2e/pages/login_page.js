c2a35241ee4e97b60fd86da4ac0b87e355eb2161
azimafnaan <azim.afnaan@hotmail.com>
2022-10-06 13:09:44 +0600

    added object variable

--- cypress/e2e/pages/login_page.js
+++ cypress/e2e/pages/login_page.js
@@ -1,12 +1,17 @@
 export class LoginPage {
 
+
+    username_textBox = '[name="username"]'
+    password_textBox = '[name="password"]'
+    login_button = '.oxd-button'
+
     enterUsername (userId) {
-        cy.get('[name="username"]').type(userId)
+        cy.get(this.username_textBox).type(userId)
     }
     enterPassword (userPassword) {
-        cy.get('[name="password"]').type(userPassword)
+        cy.get(this.password_textBox).type(userPassword)
     }
     clickLogin () {
-        cy.get('.oxd-button').click()
+        cy.get(this.login_button).click()
     }
 }
